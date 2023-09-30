16ddf4eeb91b32dfacaf28fa38c319610d3ca01c
benjaminwgordon <benjaminwillardgordon@gmail.com>
2023-09-29 21:06:08 -0500

    controller init two routes

---
+++ src/receipt/receipt.controller.ts
@@ -0,0 +1,27 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { ReceiptService } from './receipt.service';
+import { CreateReceiptDto } from './dto/create-receipt.dto';
+import { UpdateReceiptDto } from './dto/update-receipt.dto';
+
+@Controller('receipt')
+export class ReceiptController {
+  constructor(private readonly receiptService: ReceiptService) {}
+
+  @Post(':id/process')
+  create(@Body() createReceiptDto: CreateReceiptDto) {
+    return this.receiptService.create(createReceiptDto);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.receiptService.findOne(+id);
+  }
+}
---
+++ src/receipt/receipt.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { CreateReceiptDto } from './dto/create-receipt.dto';
+import { UpdateReceiptDto } from './dto/update-receipt.dto';
+
+@Injectable()
+export class ReceiptService {
+  create(createReceiptDto: CreateReceiptDto) {
+    return 'This action adds a new receipt';
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} receipt`;
+  }
+}
